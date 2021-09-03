8e55c795371a9252416e8f5881e47945764c12ab
Constanza Sánchez <84753363+constanzasanchez@users.noreply.github.com>
2021-09-02 21:01:08 -0300

    Add files via upload

---
+++ Clase-1/ejemplo.js
@@ -0,0 +1,9 @@
+var nombre = prompt("Puedes decirme tu nombre?");
+var nacimiento = prompt("Puedes decirme tu año de nacimiento?");
+var edad = 2021 - nacimiento;
+alert ("Hola " + nombre + "! Tienes " + edad + " años.");
+
+var numero1 = parseInt(prompt("Escribí tu número de la suerte!"));
+var numero2 = parseInt(prompt("Escribí otro número de la suerte!"));
+var resultado = numero1 + numero2;
+console.log("Sopresa: " + resultado + "!");
